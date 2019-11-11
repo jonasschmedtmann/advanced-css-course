f9d155c5b91b11549579d93ebd38a6d9618aedbb
Jerry V <jvr572@gmail.com>
2019-11-11 12:24:35 -0500

    "done😁 flexing"

---
+++ Trillo/starter/node_modules/concat/bin/concat.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const app = require('commander')
+const cfg = require('../package.json');
+const concat = require('../index');
+const path = require('path');
+
+app.version(cfg.version)
+  .option('-o, --output <file>', 'output file')
+  .description(cfg.description)
+
+app.parse(process.argv);
+
+let err = (err: Error) => console.log(err);
+let output = (o: string) => {
+  if (!app.output) {
+    console.log(o);
+  }
+};
+
+if(app.args.length) {
+  concat(app.args, app.output).then(output).catch(err)
+} else throw new Error('no files specified')
