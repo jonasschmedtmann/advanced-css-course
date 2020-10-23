02598aa867ad6a3e7e058b81dc1c94b2fbe95fa3
Jitesh Gadage <gadagejitesh16@gmail.com>
2020-10-23 20:19:00 +0530

    Adding trillo Update

---
+++ Trillo/Trillo update/javascript/app.js
@@ -0,0 +1,34 @@
+// var chatBox = document.getElementsByClassName('.user-nav__icon');
+
+// function chatBox(){
+//           alert("you clicked chat box");
+
+// }
+
+
+
+// chatBox.addEventListener(click,function(){
+//     chatBox.style.color = "black"
+// })
+ 
+var mesageIcon = document.querySelector(".user-nav__icon-box");
+var flightMenu = document.querySelector(".flight-menu");
+var hotelMenu = document.querySelector(".hotel-menu");
+var tourseMenu = document.querySelector(".tourse-menu");
+var homeMenu = document.querySelector(".home-menu");
+var lis = document.querySelectorAll("li");
+
+
+ mesageIcon.addEventListener("click", function(){
+    
+ });
+
+// flightMenu.addEventListener('click', function(){
+//     alert("you clicked flight menu!!!");
+   
+//         hotelMenu.style.background= "#333";
+//         flightMenu.style.background = "#eb2f64";
+       
+// });
+
+
