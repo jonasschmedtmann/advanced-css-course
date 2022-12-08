ae040ce98ac1fac8d38eb9ac35e735e5f40ec6c3
canasmh <canasmh@yahoo.com>
2022-12-08 17:06:01 -0500

    Download some icons from icomoon

---
+++ Trillo/starter/icomoon/style.css
@@ -0,0 +1,16 @@
+.icon {
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  stroke-width: 0;
+  stroke: currentColor;
+  fill: currentColor;
+}
+
+/* ==========================================
+Single-colored icons can be modified like so:
+.icon-name {
+  font-size: 32px;
+  color: red;
+}
+========================================== */
